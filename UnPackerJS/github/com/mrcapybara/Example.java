19011525cd199e53d404eb0033d8447611bc91cd
Marcos Santos <41646446+mrcapybara@users.noreply.github.com>
2019-03-09 20:46:43 -0300

    Update Example.java

--- UnPackerJS/github/com/mrcapybara/Example.java
+++ UnPackerJS/github/com/mrcapybara/Example.java
@@ -1,5 +1,5 @@
 /** *****************************************************************************
- * Copyright 2019 Marcos T. S. Martins (MrCapybara)
+ * Copyright 2019 Marcos Santos (MrCapybara)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
